2d9fbae378ece8dc7f48a550522acb8681251862
lenovo <zmgab@qq.com>
2021-09-09 17:00:44 +0800

    es

--- QuartzDemo/src/main/java/com/zmgab/demo/Log4jOtherDemo.java
+++ QuartzDemo/src/main/java/com/zmgab/demo/Log4jOtherDemo.java
@@ -1,7 +1,6 @@
 package com.zmgab.demo;
 
 import org.apache.log4j.Logger;
-
 /**
  * log4j.addivity.Log4jOtherDemo=true
  * log4j.additivity. mylogger1 = false
@@ -13,6 +12,8 @@ public class Log4jOtherDemo {
     private static Logger logger = Logger.getLogger("Log4jOtherDemo");
 
     public static void main(String[] args) {
-        logger.info("sdlfsdlfkjldf;j");
+        logger.debug("记录debug级别的信息");
+        logger.info("记录info级别的信息");
+        logger.error("记录error级别的信息");
     }
 }
