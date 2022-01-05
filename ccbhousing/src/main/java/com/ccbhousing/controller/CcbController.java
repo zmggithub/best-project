9886208e47fe4acb4c42c7d95385f57143508e5b
zmg <zmgab@qq.com>
2022-01-05 13:39:58 +0800

    change

--- ccbhousing/src/main/java/com/ccbhousing/controller/CcbController.java
+++ ccbhousing/src/main/java/com/ccbhousing/controller/CcbController.java
@@ -1,4 +1,4 @@
-package com.ccbhousing.ccbhousing.controller;
+package com.ccbhousing.controller;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
--- ccbhousing/src/test/java/com/ccbhousing/CcbhousingApplicationTests.java
+++ ccbhousing/src/test/java/com/ccbhousing/CcbhousingApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ccbhousing.ccbhousing;
+package com.ccbhousing;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
