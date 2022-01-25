adaf4610d5019e08b418b4f9c03496bdb9d149ca
zmghome <zmgab@qq.com>
2022-01-25 21:52:15 +0800

    mvn

--- ZmgAb/src/main/java/com/kaptchaDemo/Main.java
+++ ZmgAb/src/main/java/com/kaptchaDemo/Main.java
@@ -8,4 +8,7 @@ public class Main {
     // java -Djava.ext.dirs=./lib/ ccb.zwpt.client.YhTestClient
 
 
+    // 三.将外部jar打入本地maven仓库
+//    mvn install:install-file -Dfile=C:/Users/bianying/Desktop/ojdbc-master/ojdbc7.jar -DgroupId=com.oracle -DartifactId=ojdbc7 -Dversion=12.1.0.2 -Dpackaging=jar
+
 }
