e221fa7fb887edf0ae9d0e0d7ae0a1d56faec4be
zmgname <zmgab>
2022-08-16 21:34:43 +0800

    ApplicationContextUtil

--- NacosProject/ClientDemo/src/main/java/com/client/IndexController.java
+++ NacosProject/ClientDemo/src/main/java/com/client/IndexController.java
@@ -1,6 +1,7 @@
 package com.client;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -11,8 +12,17 @@ public class IndexController {
     @Autowired
     RestTemplate restTemplate;
 
+    @Autowired
+    ApplicationContext applicationContext;
+
     @GetMapping("index")
     public String index(){
+
+
+        Appconfig bean = applicationContext.getBean(Appconfig.class);
+        bean.restTemplate();
+
+
         /**
          * 9001  9002
          */
